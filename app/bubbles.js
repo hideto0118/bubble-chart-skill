1161166eedef31c7d0bf49ec7497a5d6dca59a9c
Hideto Yasunori <hideto0118yasunori@gmail.com>
2017-03-13 20:02:56 -0700

    Modify

--- app/bubbles.js
+++ app/bubbles.js
@@ -1,6 +1,10 @@
 (function() {
-  var height = 600;
-  var width = 1000;
+
+  var height = window.innerHeight;
+  var width = window.innerWidth;
+
+  var centerY = window.innerHeight / 2;
+  var centerX= window.innerWidth / 2;
 
   function responsivefy(svg) {
     // get container + svg aspect ratio
@@ -29,11 +33,6 @@
     }
   }
 
-// d3.select("#viz").append("svg")
-//     .attr("width", 960)
-//     .attr("height", 500)
-//     .call(responsivefy);
-
   var svg = d3.select("#chart")
     .append("svg")
     .attr("height", height)
@@ -42,18 +41,7 @@
     .append("g")
     .attr("transform", "translate(0,0)")
 
-  var defs = svg.append("defs");
-  defs.append("pattern")
-    .attr("id", "js")
-    .attr("height", "100%")
-    .attr("width", "100%")
-    .attr("patternContentUnits", "objectBoundingBox")
-    .append("image")
-    .attr("height", 1)
-    .attr("width", 1)
-    .attr("preserveAspectRation", "none")
-    .attr("xmls:xlink", "http://www.w3.org/1999/xlink")
-    .attr("xlink:href", "img/js.png");
+  // var defs = svg.append("defs");
 
   var radiusScale = d3.scaleSqrt().domain([0, 100]).range([10, 60])
 
@@ -76,7 +64,7 @@
 
   function ready (error, datapoints) {
 
-    defs.selectAll(".skill-pattern")
+    var defs = svg.selectAll(".skill-pattern")
       .data(datapoints)
       .enter().append("pattern")
       .attr("class", "skill-pattern")
@@ -95,6 +83,22 @@
         return "img/" + d.image;
       });
 
+
+    // var count = 0;
+    // var interval = setInterval(function(){
+    //   console.log(count);
+    //   ++count;
+    //   var body = document.querySelector("body");
+    //   if(body.onload == true) {
+
+    // window.onload = function(){
+    //   console.log("leaded!");
+
+    // document.addEventListener('DOMContentLoaded', function() {
+    //   console.log("leaded!");
+    var id;
+    var radius;
+    var image;
     var circles = svg.selectAll(".skill")
       .data(datapoints)
       .enter().append("circle")
@@ -105,24 +109,60 @@
       .attr("fill", function(d) {
         return "url(#" + d.id + ")"
       })
-      .on('click', function(d) {
-        console.log(d);
+      // .on('click', function(d) {
+      //   this.setAttribute("style", "transform: scale(1, 1);");
+      //   var thisNode = this;
+      //   setTimeout(function(){
+      //     console.log(thisNode);
+      //     thisNode.setAttribute("cx", centerX);
+      //     thisNode.setAttribute("cy", centerY);
+      //   }, 500);
+      //   setTimeout(function(){
+      //     console.log(thisNode);
+      //     thisNode.setAttribute("style", "transform: scale(4, 4);");
+      //   }, 800);
+      // })
+      .on('mouseover', function(d) {
+        var thisNode = this;
+        this.setAttribute("style", "transform: scale(2, 2);");
+        var skill = document.querySelectorAll(".skill");
+        document.querySelector("#js-text").innerHTML = d.skill;
+        skill.forEach(function(item) {
+          if(thisNode != item){
+            item.setAttribute("opacity", "0.1");
+          }
+        })
+      })
+      .on('mouseleave', function(d) {
+        this.setAttribute("style", "transform: scale(1, 1);");
+        document.querySelector("#js-text").innerHTML = "";
+        var skill = document.querySelectorAll(".skill");
+        skill.forEach(function(item) {
+          item.setAttribute("opacity", "1");
+        })
       })
 
+      // window.onload = function(){
+      //  console.log("leaded!");
 
-    simulation.nodes(datapoints)
-      .on('tick', ticked)
+      simulation.nodes(datapoints)
+          .on('tick', ticked)
+      // clearInterval(interval);
 
-    function ticked() {
-      circles
-        .attr("cx", function(d) {
-          return d.x
-        })
-        .attr("cy", function(d) {
-          return d.y
-        })
-    }
+      function ticked() {
+        circles
+          .attr("cx", function(d) {
+            return d.x
+          })
+          .attr("cy", function(d) {
+            return d.y
+          })
+      }//ticked
+    //   }//if
+    // }, 1000);
 
-  }
-
+  // }//onload
+  // }, false);//DOMContentLoaded
+// },500);
+}//ready
 })();
